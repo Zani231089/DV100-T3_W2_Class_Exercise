74579dafd1de8fcd6b8500c1e2eab4199c3d4db7
Zani231089 <231089@virtualwindow.co.za>
2023-07-29 20:53:05 +0200

    Add features to seond slide and add jquery

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,25 @@ $(document).ready(function(){
 
     console.log("Hello");
 
+    //Home Page
+
     //When the Hero image loads,animate it upwards
     $(".hero-image").animate({top: '-=100px'})
 
+    //Browse Page
+
+    $("#descriptionText").hide();
+
 });
+
+//When the card is clicked
+
+$(".card").click(function(){
+
+    //Toggle price and description text
+    $("#pricetext").toggle();
+    $("#descriptionText").toggle();
+
+    //Resize the image to fit the additional content
+    $(".car-img-top").toggleClass("small");
+})
